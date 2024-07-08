80e19e7afda2fea530a102aad1d888eca0b1fc24
THANKSHANK <1594556308@qq.com>
2024-07-08 10:11:01 -0400

    ray tracing update

--- vertex.py
+++ vertex.py
@@ -3,14 +3,6 @@ This file contains the Vertex class, which is used to represent a vertex in the 
 """
 __author__ = "Zihan Wang"
 class Vertex:
-    # def __init__(self, x, y, z, r, g, b):
-    #     self.x = x
-    #     self.y = y
-    #     self.z = z
-    #     self.r = r
-    #     self.g = g
-    #     self.b = b
-
     def __init__(self,id):
         self._id = id
         self._varyings = {}
